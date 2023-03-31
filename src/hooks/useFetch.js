2da4d49947bff8c7bc07d237f955e54f03cdc9a4
rahul <rahulor2000@gmail.com>
2023-03-31 22:53:05 +0530

    signup finished

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import axios from 'axios'
+
+export default function useFetch(method) {
+
+    let BASEURL = "http://localhost:5000/api";
+    let METHOD = null;
+
+    switch (method) {
+        case "GET":
+            METHOD = "GET"
+            break;
+        case "POST":
+            METHOD = "POST"
+            break;
+        case "PUT":
+            METHOD = "PUT"
+            break;
+        case "DELETE":
+            METHOD = "DELETE"
+            break;
+        case "PATCH":
+            METHOD = "PATCH"
+            break;
+        default:
+            METHOD = null;
+    }
+
+    console.log(BASEURL);
+
+    return function fetchData(url, data) {
+        if(!BASEURL.endsWith(url))
+            BASEURL = BASEURL.concat(url);
+        
+        return new Promise((resolve, reject) => {
+            if (METHOD == null) {
+                reject("method is not applicable");
+            }
+            console.log(data);
+
+            try {
+                axios({
+                    method: METHOD,
+                    url: BASEURL,
+                    data: JSON.stringify(data),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${JSON.parse(localStorage.getItem('user-token')) || ""}`
+                    }
+                }).then(res => {
+                    resolve(res.data);
+                }).catch(err => {
+                    console.log(err.response.data.message);
+                    reject(err.response.data.message);
+                })
+            } catch (error) {
+                reject(error.message);
+            }
+        })
+    }
+}

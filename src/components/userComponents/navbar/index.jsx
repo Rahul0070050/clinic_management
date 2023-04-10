ddbb919c9e044769bc25ddc351ebf890a9f5283d
rahul <rahulor2000@gmail.com>
2023-04-10 11:04:28 +0530

    navbar css removed

--- src/components/userComponents/navbar/index.jsx
+++ src/components/userComponents/navbar/index.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function UserNavBar() {
   return (
-    <div>UserNavBar</div>
+    <nav>
+    <div className="logo">
+      Skin Care
+    </div>
+    <div className="links">
+      <span>Help</span>
+      <span>User</span>
+      <span>User</span>
+    </div>
+  </nav>
   )
 }
 

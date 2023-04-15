d2e457f556df197075d7c9a7c0e063778bf9ba6d
rahul <rahulor2000@gmail.com>
2023-04-15 11:31:37 +0530

    login finished

---
+++ src/components/userComponents/footer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './style.scss'
+
+function index() {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default index
--- src/pages/doctor/login/index.jsx
+++ src/pages/doctor/login/index.jsx
@@ -1,90 +1,132 @@
-import React, { useState } from 'react';
-
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 import useFetch from '../../../hooks/useFetch';
 
-import userLogo from '../../../assets/images/user-login-logo.png';
-import openEye from '../../../assets/images/white-open-eye.png';
-import closedEYe from '../../../assets/images/white-closed-eye.png';
+import openEye from '../../../assets/images/open-eye.png';
+import closedEYe from '../../../assets/images/closed-eye.png';
+import sideImage from '../../../assets/svg/login-page-logo.svg'
 
 import './style.scss';
-import { Link, useNavigate } from 'react-router-dom';
 
 function Login() {
-    const navigate = useNavigate();
+  const [userData, setUserData] = useState({ username: "", password: "", confirm_password: "" });
+  const [userDataErr, setUserDataErr] = useState({ username: "", password: "", confirm_password: "" });
+  const [showPassword, setShowPassword] = useState(false);
 
-    const [userData, setUserData] = useState({ username: "", password: "" });
-    const [userDataErr, setUserDataErr] = useState({ username: false, password: false });
-    const [showPassword, setShowPassword] = useState(false);
-    const [err, setErr] = useState(null);
-
-    const postRequest = useFetch("POST");
+  const navigate = useNavigate()
+  const postRequest = useFetch("POST");
 
 
-    function handleOnchange(e) {
-        setUserData(prev => {
+  function handleOnchange(e) {
+    setUserData(prev => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (userData.username == "" || userData.password == "" || userData.confirm_password == "") {
+      for (const key in userData) {
+        if (userData[key] == "") {
+          setUserDataErr((prev) => {
             return {
-                ...prev,
-                [e.target.name]: e.target.value
+              ...prev,
+              [key]: "place provide " + key
             }
-        })
+          })
+        } else {
+          setUserDataErr((prev) => {
+            return {
+              ...prev,
+              [key]: ""
+            }
+          })
+        }
+      }
+      return;
     }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-
+    if (userData.password != userData.confirm_password || userData.password.length < 8) {
+      if (userData.password.length < 8) {
         setUserDataErr(prev => {
-            return {
-                ...prev,
-                username: userData.username == "" ? true : false,
-                password: userData.password == "" ? true : false
-            }
+          return {
+            ...prev,
+            password: "password must be more than 8 characters"
+          }
+        });
+      }
+
+      if (userData.password != userData.confirm_password) {
+        setUserDataErr(prev => {
+          return {
+            ...prev,
+            confirm_password: "password is not matching"
+          }
         })
-        if (userData.username == "" || userData.password == "") {
-            return;
-        }
-
-        if (userData.password.length < 8) {
-            setErr(prev => "password should be at least 8 characters");
-            return
-        }
-        console.log(userData);
-
-        try {
-
-            postRequest('/doctor/login', userData).then(res => {
-                console.log(res);
-                localStorage.setItem('doctor-token', JSON.stringify(res.token))
-                navigate('/doctor')
-            }).catch(err => {
-                setErr(prev => err)
-            });
-        } catch (error) {
-            console.log(error);
-        }
-
+      }
+      return;
     }
-    return (
-        <div className="doctor-login">
-            <form>
-                <div className="logo">
-                    <img src={userLogo} alt="" />
-                </div>
-                <h2>Login</h2>
-                <div className="form-control">
-                    <label htmlFor="username">username {userDataErr.username ? "* please provide username" : ""}</label>
-                    <input onChange={handleOnchange} type="text" name="username" id="username" />
-                </div>
-                <div className="form-control">
-                    <label htmlFor="password">password {userDataErr.password ? "* please provide password" : ""}</label>
-                    <input onChange={handleOnchange} type={showPassword ? "text" : "password"} name="password" id="password" />
-                    <img onClick={() => setShowPassword(pres => !pres)} src={showPassword ? openEye : closedEYe} alt="" />
-                </div>
-                <button onClick={handleSubmit} type='button'>submit</button>
-                <Link to='/forgot-password'>forgot password</Link>
-                {err && <div className='error-message'>{err}</div>}
-            </form>
-        </div>
-    )
+
+
+    setUserDataErr({ username: "", password: "", confirm_password: "" })
+
+
+    try {
+
+      postRequest('/doctor/login', userData).then(res => {
+        console.log(res);
+        localStorage.setItem('doctor-token', JSON.stringify(res.token))
+        navigate('/doctor')
+      }).catch(err => {
+        setUserDataErr(prev => {
+          return {
+            ...prev,
+            ...err,
+          }
+        })
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  return (
+    <div className='user-login'>
+      <div className="info">
+        <h1>Doctor Sign In</h1>
+        <h5>Please Login To Continue</h5>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since.</p>
+        <form>
+          <div className="form-control">
+            <label htmlFor="username">username {userDataErr.username && <span>*{userDataErr.username}</span>}</label>
+            <input type="text" name="username" onChange={handleOnchange} id="username" />
+          </div>
+          <div className="form-control">
+            <img src={showPassword ? openEye : closedEYe} onClick={() => setShowPassword(show => !show)} alt="" />
+            <label htmlFor="password">password {userDataErr.password && <span>*{userDataErr.password}</span>}</label>
+            <input type={showPassword ? "text" : "password"} name="password" onChange={handleOnchange} id='password' />
+          </div>
+          <div className="form-control">
+            <label htmlFor="confirm-password">confirm password {userDataErr.confirm_password && <span>*{userDataErr.confirm_password}</span>}</label>
+            <input type={showPassword ? "text" : "password"} id='confirm-password' onChange={handleOnchange} name="confirm_password" />
+          </div>
+          <div className="form-control">
+            <Link to="/signup">I don't have an account</Link>
+            <input type="button" name="button" onClick={handleSubmit} value="Sign In" />
+          </div>
+        </form>
+      </div>
+      <div className="image">
+        <img src={sideImage} alt="" />
+      </div>
+    </div>
+  )
 }
 
-export default Login;
+export default Login
